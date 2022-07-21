15739bad3bafcc17c93c9fff9995729648caad2d
dpizapal <dpizapal@yahoo.es>
2022-07-21 22:02:04 +0000

    footer

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center bg-dark text-warning">
+		<p>
+          “Patience you must have my young Padawan.” — Yoda
+			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		</p>
+	</footer>
+);
+export default Footer
